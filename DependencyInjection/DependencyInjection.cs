fdc7aca93ef00d80e90f098796d997b303210678
Ruben Morales <rm372718@gmail.com>
2023-05-11 15:35:57 -0600

    added_dependency_injection_features

---
+++ DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MySolution.Infraestructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySolution.Domain.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterSolutionResources(this IServiceCollection services, string dbConection)
+        {
+            services.AddDbContext<MySolutionDataContext>(options => options.UseSqlServer(dbConection, b => b.MigrationsAssembly("MySolution.Infraestructure")));
+            services.AddScoped<DbContext, MySolutionDataContext>();
+
+            return services;
+        }
+    }
+}
---
+++ MySolution.WebAPI/Controllers/MusicsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySolution.Domain.Infraestructure;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MySolution.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class MusicsController : ControllerBase
+    {
+        private readonly IMusicService _musicService;
+        // GET: api/<MusicsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/<MusicsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MusicsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MusicsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
